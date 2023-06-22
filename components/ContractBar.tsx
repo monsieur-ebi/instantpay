c72ec880cda93920f63da5362355483dd3d04fad
monsieur-ebi <ebizimordisi123@gmail.com>
2023-06-22 05:27:11 -0700

    Fixed Errors

--- components/ContractBar.tsx
+++
@@ -1,9 +0,0 @@
-const contractBar = () => {
-  return (    
-    <input className="w-auto rounded-lg md:w-[1008px] bg-[#F5F5F5] p-4 text-[#989898] text-center focus:border-0" type="text" 
-    placeholder="Search: Transactions , block,address and private key..." 
-    name="" id="" />
-  )
-}
-
-export default contractBar
