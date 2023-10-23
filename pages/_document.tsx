f322bb9c234f297dc12b3e7c9486ee715b7b8cf4
Disi Ebizimor Resource <ebizimordisi123@gmail.com>
2023-10-23 01:48:45 +0100

    Update _document.tsx
    Added new live chat code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="//code.tidio.co/lqjprsrfx5cyeqpqimx1uabrvipo1iar.js" async></script>
+        <script src="//code.tidio.co/zhlrniudufohreydku3wfx4bjjo4bfb4.js" async></script>
       </body>
     </Html>
   )
