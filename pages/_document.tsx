5f573f91a9ce35df2e0264c1b7a380a5d069b695
Disi Ebizimor Resource <ebizimordisi123@gmail.com>
2024-06-06 12:42:11 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,22 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'a090b4fded29670d18839406e91d55fbfdcf3870';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <script src="//code.tidio.co/4rqznmokaqegovgbs8se5hsyjniczylo.js" async></script>
       </body>
     </Html>
   )
